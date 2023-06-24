e13bd27456f0f1d9043b4d5daadd52f0133024c3
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-24 20:36:52 +0530

    update

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes,Route } from 'react-router';
+import Welcome from './Welcome';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <><Home /><Routes>
+      <Route exact path="/" element={<Welcome></Welcome>} />
+    </Routes></>
+  );
+}
+
---
+++ myapp/src/components/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return (
+        <h2>Home here</h2>
+    )
+}
+
+export default Home;

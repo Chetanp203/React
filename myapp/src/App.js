76667e13bea1120e6b7637ba8839d828bff7ff9a
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-25 15:36:13 +0530

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes,Route } from 'react-router';
+import { Routes, Route } from 'react-router';
 import Welcome from './Welcome';
 import Home from './components/Home';
 
 function App() {
   return (
-    <><Home /><Routes>
-      <Route exact path="/" element={<Welcome></Welcome>} />
+    <>
+    <Home />
+    <Routes>
+      <Route exact path="/" element={<Welcome />} />
     </Routes></>
   );
 }
 
+export default App
